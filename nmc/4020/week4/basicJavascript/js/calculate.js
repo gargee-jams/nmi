8980fb1268b4f05302e76c49da25b5ea55d3f3d1
gargee-jams <104339886+gargee-jams@users.noreply.github.com>
2023-01-30 10:00:27 -0500

    operators

--- nmc/4020/week4/basicJavascript/js/calculate.js
+++ nmc/4020/week4/basicJavascript/js/calculate.js
@@ -1,5 +1,6 @@
 let input1 = document. querySelector('#input1');
 let input2 = document.querySelector('#input2');
+let op = document.querySelector('#operator');
 let addButton = document.querySelector('#addButton');
 let subButton = document.querySelector('#subButton');
 let multButton = document.querySelector('#multButton');
@@ -7,15 +8,19 @@ let divButton = document.querySelector('#divButton');
 let output = document.querySelector('#output');
 
 function add() {
+    op.value == "+";
     output.value = "$" + Number(input1.value) + Number(input2.value);
 }
 function sub() {
+    op.value == "-";
     output.value = "$" + Number(input1.value) - Number(input2.value);
 }
 function mult() {
+    op.value == "*";
     output.value = "$" + Number(input1.value) * Number(input2.value);
 }
 function div() {
+    op.value == "/";
     output.value = "$" + Number(input1.value) / Number(input2.value);
 }
 addButton.onclick = () => add();
