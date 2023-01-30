55e3a89348bfbd91c709b4c83fa09b139adc01a8
gargee-jams <104339886+gargee-jams@users.noreply.github.com>
2023-01-30 10:21:20 -0500

    Update calculate.js

--- nmc/4020/week4/basicJavascript/js/calculate.js
+++ nmc/4020/week4/basicJavascript/js/calculate.js
@@ -1,22 +1,24 @@
 let input1 = document. querySelector('#input1');
 let input2 = document.querySelector('#input2');
+
 let addButton = document.querySelector('#addButton');
 let subButton = document.querySelector('#subButton');
 let multButton = document.querySelector('#multButton');
 let divButton = document.querySelector('#divButton');
+
 let output = document.querySelector('#output');
 
 function add() {
-    output.value = "$" + Number(input1.value) + Number(input2.value);
+    output.value = "$" + (Number(input1.value) + Number(input2.value));
 }
 function sub() {
-    output.value = "$" + Number(input1.value) - Number(input2.value);
+    output.value = "$" + (Number(input1.value) - Number(input2.value));
 }
 function mult() {
-    output.value = "$" + Number(input1.value) * Number(input2.value);
+    output.value = "$" + (Number(input1.value) * Number(input2.value));
 }
 function div() {
-    output.value = "$" + Number(input1.value) / Number(input2.value);
+    output.value = "$" + (Number(input1.value) / Number(input2.value));
 }
 addButton.onclick = () => add();
 subButton.onclick = () => sub();
