864a8c69b31e3cd4e054f6d7092295fe3cf24274
gargee-jams <104339886+gargee-jams@users.noreply.github.com>
2023-02-08 11:08:55 -0500

    Update candidates.js

--- nmc/4020/week5/candidates/js/candidates.js
+++ nmc/4020/week5/candidates/js/candidates.js
@@ -2,4 +2,6 @@ let candidateNames = ["Jeb Bush","Ben Carson","Lincoln Chafee","Chris Christie",
 let candidatePix = ["bush.jpg","carson.jpg","chafee.jpg","christie.jpg","cruz.jpg","fiorina.jpg","gilmore.jpg","graham.jpg","jindal.jpg","kasich.jpg","lessig.jpg","omalley.jpg","pataki.jpg","paul.jpg","perry.jpg","rubio.jpg","sanders.jpg","santorum.jpg","trump.jpg","walker.jpg","webb.jpg"]
 let output = document.querySelector('#alsoRan');
 //output.innerHTML = candidateNames[18];
-output.innerHTML = candidateNames[0] + "<img src=pix/" + candidatePix[0] + ">";
+for (var i = 0; i < candidateNames.length - 1; i++) {
+    output.innerHTML += candidateNames[i] + "<br><img src=pix/" + candidatePix[i] + "> <br>";
+} //for
