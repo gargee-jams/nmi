a1524d4eb3582705016b1258d09a0611142424e6
gargee-jams <104339886+gargee-jams@users.noreply.github.com>
2023-02-13 10:28:29 -0500

    conditional

---
+++ nmc/4020/week6/basicJavascript/conditional.js
@@ -0,0 +1,7 @@
+let answer = 24;
+let reaction = document.querySelector('#response');
+if (answer == 24) {
+    reaction.innerHTML = "correct";
+} else {
+    reaction.innerHTML = "wrong answer";
+} //if
