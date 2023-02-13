3f67d9f2cca4aea828533f6896b0e6742fb27b59
gargee-jams <104339886+gargee-jams@users.noreply.github.com>
2023-02-13 09:56:59 -0500

    2

--- nmc/4020/week5/candidates/js/candidates2.js
+++ nmc/4020/week5/candidates/js/candidates2.js
@@ -1,7 +1,19 @@
 let candidateNames = ["Jeb Bush","Ben Carson","Lincoln Chafee","Chris Christie","Ted Cruz","Carly Fiorina","Jim Gilmore","Lindsey Graham","Bobby Jindal","John Kasich","Lawrence Lessig","Martin O'Malley","George Pataki","Rand Paul","Rick Perry","Marco Rubio","Bernie Sanders","Rick Santorum","Donald Trump","Scott Walker","Jim Webb"]
 let candidatePix = ["bush.jpg","carson.jpg","chafee.jpg","christie.jpg","cruz.jpg","fiorina.jpg","gilmore.jpg","graham.jpg","jindal.jpg","kasich.jpg","lessig.jpg","omalley.jpg","pataki.jpg","paul.jpg","perry.jpg","rubio.jpg","sanders.jpg","santorum.jpg","trump.jpg","walker.jpg","webb.jpg"]
 let output = document.querySelector('#alsoRan');
-//output.innerHTML = candidateNames[18];
+let next = document.querySelector('#next');
+let previous = document.querySelector('#previous');
 for (var i = 0; i < candidateNames.length - 1; i++) {
     output.innerHTML += "<li>" + candidateNames[i] + "<br><img src=pix/" + candidatePix[i] + "> </li>";
 } //for
+let i = 0;
+function canShow(plusMinus) {
+    output.innerHTML = "<li>" + candidateNames[i] + "<br><img src=pix/" + candidatePix[i] + "> </li>";
+    i += plusMinus;
+} //canShow
+next.onclick = function() {
+    canShow(1);
+} //onClick
+previous.onclick = function() {
+    canShow(-1);
+} //onClick
