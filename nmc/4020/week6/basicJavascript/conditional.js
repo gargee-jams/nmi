74ef2207b2b8279d37dbea65e3e65038ad88fbea
gargee-jams <104339886+gargee-jams@users.noreply.github.com>
2023-02-15 09:28:38 -0500

    con

--- nmc/4020/week6/basicJavascript/conditional.js
+++ nmc/4020/week6/basicJavascript/conditional.js
@@ -1,10 +1,37 @@
 function compare () {
     let answer = document.querySelector('#userInput');
     let reaction = document.querySelector('#response');
-    answer = Number(answer.value);
-    if (answer == 24) {
+    answer = answer.value;
+    answer = Number(answer);
+
+    switch (true) {
+        case (answer == 24):
+            reaction.innerHTML = "correct";
+            break;
+        case (answer > 24) :
+                reaction.innerHTML = answer + " is too high";
+                break;
+        case ((answer > 24) && (answer <= 28)) :
+                reaction.innerHTML = answer + " is too high but pretty close";
+                break;
+        case (answer < 24) :
+                reaction.innerHTML = answer + " is too low";
+                break;
+        case ((answer < 24) && (answer >= 20)) :
+                reaction.innerHTML = answer + " is too low but pretty close";
+                break;
+    } //switch
+    
+    /*if (answer == 24) {
         reaction.innerHTML = "correct";
-    } else {
-        reaction.innerHTML = "wrong answer";
-    } //if
+    } else if (answer > 24) {
+        reaction.innerHTML = answer + " is too high";
+    } else if ((answer > 24) && (answer <= 28)) {
+        reaction.innerHTML = answer + " is too high but pretty close";
+    } else if (answer < 24) {
+        reaction.innerHTML = answer + " is too low";
+    } else if ((answer < 24) && (answer >= 20)) {
+        reaction.innerHTML = answer + " is too low but pretty close";
+    }//if
+    */
 } //compare
