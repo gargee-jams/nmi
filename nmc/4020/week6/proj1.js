804988c795840e0c81f597ba6840be522d9ef3e2
gargee-jams <104339886+gargee-jams@users.noreply.github.com>
2023-02-15 21:51:34 -0500

    Update proj1.js

--- nmc/4020/week6/proj1.js
+++ nmc/4020/week6/proj1.js
@@ -1,22 +1,43 @@
-let candidateNames = ["Jeb Bush","Ben Carson","Lincoln Chafee","Chris Christie","Ted Cruz","Carly Fiorina","Jim Gilmore","Lindsey Graham","Bobby Jindal","John Kasich","Lawrence Lessig","Martin O'Malley","George Pataki","Rand Paul","Rick Perry","Marco Rubio","Bernie Sanders","Rick Santorum","Donald Trump","Scott Walker","Jim Webb"]
-let candidatePix = ["bush.jpg","carson.jpg","chafee.jpg","christie.jpg","cruz.jpg","fiorina.jpg","gilmore.jpg","graham.jpg","jindal.jpg","kasich.jpg","lessig.jpg","omalley.jpg","pataki.jpg","paul.jpg","perry.jpg","rubio.jpg","sanders.jpg","santorum.jpg","trump.jpg","walker.jpg","webb.jpg"]
+let candidateNames1 = ["Jeb Bush","Ben Carson","Lincoln Chafee","Chris Christie","Ted Cruz","Carly Fiorina","Jim Gilmore"];
+let candidateNames2 = ["Lindsey Graham","Bobby Jindal","John Kasich","Lawrence Lessig","Martin O'Malley","George Pataki","Rand Paul"];
+let candidateNames3 = ["Rick Perry","Marco Rubio","Bernie Sanders","Rick Santorum","Donald Trump","Scott Walker","Jim Webb"];
+let candidatePix1 = ["bush.jpg","carson.jpg","chafee.jpg","christie.jpg","cruz.jpg","fiorina.jpg","gilmore.jpg"];
+let candidatePix2 = ["graham.jpg","jindal.jpg","kasich.jpg","lessig.jpg","omalley.jpg","pataki.jpg","paul.jpg"];
+let candidatePix3 = ["perry.jpg","rubio.jpg","sanders.jpg","santorum.jpg","trump.jpg","walker.jpg","webb.jpg"];
+
 let output = document.querySelector('#alsoRan');
-let next = document.querySelector('#next');
-let previous = document.querySelector('#previous');
+let s1Add = document.querySelector('#ss1Add');
+let s1Sub = document.querySelector('#ss1Sub');
+let s2Add = document.querySelector('#ss2Add');
+let s2Sub = document.querySelector('#ss2Sub');
+let s3Add = document.querySelector('#ss3Add');
+let s3Sub = document.querySelector('#ss3Sub');
 
 let i = 0;
-function canShow(plusMinus) {
-    output.innerHTML = "<li>" + candidateNames[i] + "<br><img src=pix/" + candidatePix[i] + "> </li>";
+function canShow(plusMinus, arr1, arr2) {
+    output.innerHTML = "<li>" + arr1[i] + "<br><img src=pix/" + ar22[i] + "> </li>";
     i += plusMinus;
-    if (i == candidateNames.length) {
+    if (i == arr1.length) {
         i = 0;
     } else if (i < 0) {
-        i = candidateNames.length - 1;
+        i = arr1.length - 1;
     } //if
 } //canShow
-next.onclick = function() {
-    canShow(1);
+s1Add.onclick = function() {
+    canShow(1, candidateNames1, candidatePix1);
 } //onClick
-previous.onclick = function() {
-    canShow(-1);
+s1Sub.onclick = function() {
+    canShow(-1, candidateNames1, candidatePix1);
+} //onClick
+s2Add.onclick = function() {
+    canShow(1, candidateNames2, candidatePix2);
+} //onClick
+s2Sub.onclick = function() {
+    canShow(-1, candidateNames2, candidatePix2);
+} //onClick
+s3Add.onclick = function() {
+    canShow(1, candidateNames3, candidatePix3);
+} //onClick
+s3Sub.onclick = function() {
+    canShow(-1, candidateNames3, candidatePix3);
 } //onClick
