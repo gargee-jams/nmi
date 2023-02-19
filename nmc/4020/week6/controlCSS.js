ab5a7b84c7319f3ca995377894aeee39f16b0101
gargee-jams <104339886+gargee-jams@users.noreply.github.com>
2023-02-18 22:14:20 -0500

    Update controlCSS.js

--- nmc/4020/week6/controlCSS.js
+++ nmc/4020/week6/controlCSS.js
@@ -3,6 +3,6 @@ let sq = document.querySelector('#square');
 function changeCSS() {
    // sq.style.backgroundColor = "red";
     sq.style.borderRadius = "90%";
-    sq.style.backgroundImage = url("https://media.tenor.com/zrpyKEyxZGwAAAAM/fat-cat-laser-eyes.gif");
+    sq.style.backgroundImage = "url('https://media.tenor.com/zrpyKEyxZGwAAAAM/fat-cat-laser-eyes.gif')";
 
 } //changeCSS
