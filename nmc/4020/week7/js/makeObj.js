249450144d30cf6739e8d6b8eedbbe0193f31b04
gargee-jams <104339886+gargee-jams@users.noreply.github.com>
2023-02-22 00:09:27 -0500

    make

---
+++ nmc/4020/week7/js/makeObj.js
@@ -0,0 +1,23 @@
+let classObject = {"AlexW": "I am the oldest of two boys, two girls; two redheads, two blondes; two left-handers, two right-handers. ",
+"AlexC": " My beard is thicker than this photo shows. I need new pictures :-(",
+"Allison": "I write about peanuts. ",
+"Amy": " I like mint tea and taking pictures!",
+"aardvark": " I like to eat peanuts. "}
+
+let nameArray =["AlexW","AlexC","Allison","Amy","aardvark"];
+ 
+let nameButtons = document.querySelector("#aname");
+for (let i = 0; i < nameArray.length; i++) {
+    nameButtons.innerHTML += "<button id=" + nameArray[i] + " onclick =quote('" + nameArray[i] + "')>nameArray[i]</button>";
+} //for
+
+
+function quote(name){
+    let sName = document.querySelector("#aname").value;
+    
+    if (classObject.hasOwnProperty(sName)) {
+      document.querySelector("#about").innerHTML = classObject[sName];
+    } else {
+      document.querySelector("#about").innerHTML = "No information available about " + sName;
+    } //if
+  } //quote
