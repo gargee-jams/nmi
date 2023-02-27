afae2d2cbd700b87e1184df8cb759a483145ef18
gargee-jams <104339886+gargee-jams@users.noreply.github.com>
2023-02-27 10:25:53 -0500

    Update makeObj.js

--- nmc/4020/week7/js/makeObj.js
+++ nmc/4020/week7/js/makeObj.js
@@ -4,23 +4,24 @@ let classObject = {"Ocean Fantasy": "ocean",
 "Cloud Fantasy": "cloud",
 "Mountain Fantasy": "mountain",
 }
+let value = classObject[""];
+
 
 let keywordArray =["Ocean Fantasy","Space Fantasy","Forest Fantasy","Cloud Fantasy","Mountain Fantasy"];
  
 let keywordButtons = document.querySelector("#artKeywords");
 for (let i = 0; i < keywordArray.length; i++) {
-  keywordButtons.innerHTML += "<li><button onclick = quote('" + classObject[i] + "') value = " + classObject[i] +" >" + keywordArray[i]+"</button></li>";
+  keywordButtons.innerHTML += "<li><button onclick = quote('" + classObject[keywordArray[i]] + "') >" + keywordArray[i]+"</button></li>";
 } //for
 
 
 function quote(keyword){   
-    //let source = classObject[document.querySelector("#artName").value];
-   //let source = classObject[keyword];
-    let source = document.querySelector("#artKeywords").value;
+
+    //let source = document.querySelector("#artKeywords").value;
 
     if (classObject.hasOwnProperty(keyword)) {
-      document.querySelector("#about").innerHTML = "No information available about " + source;
-      document.querySelector("#image").innerHTML = "<img src= pix/" + source + ".jpg>";
+      document.querySelector("#about").innerHTML = "No information available about " + keyword;
+      document.querySelector("#image").innerHTML = "<img src= pix/" + keyword + ".jpg>";
     } else {
       document.querySelector("#about").innerHTML = "No information available about " + keyword;
     } //if
